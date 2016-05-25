8ed9983c53b719cc78cf6ea955347fae74134598
RaISug <radoslav.i.sugarev@gmail.com>
2016-05-25 22:06:18 +0300

    Add default admin view

---
+++ application/views/vadmin.php
@@ -0,0 +1,15 @@
+<?php 
+
+	include 'admin/avheader.php';	
+
+?>
+
+	<div class="container">
+		<h1 style="text-align: center">Администраторски панел</h1>
+	</div>
+	
+<?php
+	
+	include 'admin/avfooter.php';
+	
+?>
