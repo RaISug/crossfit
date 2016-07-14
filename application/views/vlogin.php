c218350520f86aa146e51b1f6e01953b094b8b13
Sugarev <Sugarev@Sugarevi>
2016-07-14 20:00:52 +0300

    Add header and footer to the login and the registration form
    Change-Id: I5e26717056d1a4d59ca33f46177bd611c1161aaa
    Signed-off-by: Sugarev <Sugarev@Sugarevi>

--- application/views/vlogin.php
+++ application/views/vlogin.php
@@ -1,55 +1,58 @@
-<style>
-	body {
-		padding-top: 40px;
-		padding-bottom: 40px;
-		background-color: #eee;
-	}
-	
-	.form-signin {
-		max-width: 330px;
-		padding: 35px;
-		margin: 0 auto;
-		box-shadow: 5px 5px 15px gray;
-	}
-	
-	.form-signin .form-signin-heading, .form-signin .checkbox {
-		margin-bottom: 10px;
-	}
-	
-	.form-signin .checkbox {
-		font-weight: normal;
-	}
-	
-	.form-signin .form-control {
-		position: relative;
-		height: auto;
-		-webkit-box-sizing: border-box;
-		-moz-box-sizing: border-box;
-		box-sizing: border-box;
-		padding: 10px;
-		font-size: 16px;
-	}
-	
-	.form-signin .form-control:focus {
-		z-index: 2;
-	}
-	
-	.form-signin input[type="email"] {
-		margin-bottom: -1px;
-		border-bottom-right-radius: 0;
-		border-bottom-left-radius: 0;
-	}
-	
-	.form-signin input[type="password"] {
-		margin-bottom: 10px;
-		border-top-left-radius: 0;
-		border-top-right-radius: 0;
-	}
-</style>
+<?php
 
-<link href="<?php echo base_url("assets/css/bootstrap/bootstrap.min.css"); ?>" rel="stylesheet">
+	include "vheader.php"
 
-<div>
+?>
+
+	<style>
+		body {
+			padding-bottom: 40px;
+			background-color: #eee;
+		}
+		
+		.form-signin {
+			max-width: 330px;
+			padding: 35px;
+			margin: 175px auto;
+			box-shadow: 5px 5px 15px gray;
+		}
+		
+		.form-signin .form-signin-heading, .form-signin .checkbox {
+			margin-bottom: 10px;
+		}
+		
+		.form-signin .checkbox {
+			font-weight: normal;
+		}
+		
+		.form-signin .form-control {
+			position: relative;
+			height: auto;
+			-webkit-box-sizing: border-box;
+			-moz-box-sizing: border-box;
+			box-sizing: border-box;
+			padding: 10px;
+			font-size: 16px;
+		}
+		
+		.form-signin .form-control:focus {
+			z-index: 2;
+		}
+		
+		.form-signin input[type="email"] {
+			margin-bottom: -1px;
+			border-bottom-right-radius: 0;
+			border-bottom-left-radius: 0;
+		}
+		
+		.form-signin input[type="password"] {
+			margin-bottom: 10px;
+			border-top-left-radius: 0;
+			border-top-right-radius: 0;
+		}
+	</style>
+
+	<link href="<?php echo base_url("assets/css/bootstrap/bootstrap.min.css"); ?>" rel="stylesheet">
 
 	<div class="container">
 
@@ -73,4 +76,8 @@
 		?>
 	</div>
 
-</div>
+<?php
+
+	include "vfooter.php"
+
+?>
--- application/views/vregistration.php
+++ application/views/vregistration.php
@@ -1,66 +1,69 @@
-<style>
-	body {
-		padding-top: 40px;
-		padding-bottom: 40px;
-		background-color: #eee;
-	}
-	
-	.form-registration {
-		max-width: 330px;
-		padding: 35px;
-		margin: 0 auto;
-		box-shadow: 5px 5px 15px gray;
-	}
-	
-	.form-registration .form-registration-heading, .form-registration .checkbox {
-		margin-bottom: 10px;
-	}
-	
-	.form-registration .checkbox {
-		font-weight: normal;
-	}
-	
-	.form-registration .form-control {
-		position: relative;
-		height: auto;
-		-webkit-box-sizing: border-box;
-		-moz-box-sizing: border-box;
-		box-sizing: border-box;
-		padding: 10px;
-		font-size: 16px;
-	}
-	
-	.form-registration .form-control:focus {
-		z-index: 2;
-	}
-	
-	.form-registration input[type="email"] {
-		margin-bottom: -1px;
-		border-bottom-right-radius: 0;
-		border-bottom-left-radius: 0;
-	}
-	
-	.form-registration input[type="password"] {
-		margin-bottom: -1px;
-		border-top-left-radius: 0;
-		border-top-right-radius: 0;
-	}
-	
-	.form-registration input[type="text"] {
-		margin-bottom: -1px;
-		border-bottom-right-radius: 0;
-		border-bottom-left-radius: 0;
-	}
-	
-	.form-button {
-		margin-top: 10px;
-	}
-	
-</style>
+<?php
 
-<link href="<?php echo base_url("assets/css/bootstrap/bootstrap.min.css"); ?>" rel="stylesheet">
+	include "vheader.php"
 
-<div>
+?>
+
+	<style>
+		body {
+			padding-bottom: 40px;
+			background-color: #eee;
+		}
+		
+		.form-registration {
+			max-width: 330px;
+			padding: 35px;
+			margin: 175px auto;
+			box-shadow: 5px 5px 15px gray;
+		}
+		
+		.form-registration .form-registration-heading, .form-registration .checkbox {
+			margin-bottom: 10px;
+		}
+		
+		.form-registration .checkbox {
+			font-weight: normal;
+		}
+		
+		.form-registration .form-control {
+			position: relative;
+			height: auto;
+			-webkit-box-sizing: border-box;
+			-moz-box-sizing: border-box;
+			box-sizing: border-box;
+			padding: 10px;
+			font-size: 16px;
+		}
+		
+		.form-registration .form-control:focus {
+			z-index: 2;
+		}
+		
+		.form-registration input[type="email"] {
+			margin-bottom: -1px;
+			border-bottom-right-radius: 0;
+			border-bottom-left-radius: 0;
+		}
+		
+		.form-registration input[type="password"] {
+			margin-bottom: -1px;
+			border-top-left-radius: 0;
+			border-top-right-radius: 0;
+		}
+		
+		.form-registration input[type="text"] {
+			margin-bottom: -1px;
+			border-bottom-right-radius: 0;
+			border-bottom-left-radius: 0;
+		}
+		
+		.form-button {
+			margin-top: 10px;
+		}
+		
+	</style>
+
+	<link href="<?php echo base_url("assets/css/bootstrap/bootstrap.min.css"); ?>" rel="stylesheet">
 
 	<div class="container">
 
@@ -86,7 +89,9 @@
 		</form>
 
 	</div>
-	<!-- /container -->
 
+<?php
 
-</div>
+	include "vfooter.php"
+
+?>
